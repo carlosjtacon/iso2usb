2da13cfe0eaa765768aab75d071c3a34aad34229
Carlos Tacón <carlosjtacon@gmail.com>
2014-08-03 16:37:21 +0200

    iso to usb python script

---
+++ iso2usb.py
@@ -0,0 +1,24 @@
+import os, sys
+
+if len(sys.argv) != 2:
+	sys.exit('Usage: sudo %s path-to-iso' % sys.argv[0])
+
+if not os.path.exists(sys.argv[1]):
+    sys.exit('ERROR: ISO file %s was not found!' % sys.argv[1])
+
+print 'Locate the disk identifier in the following list:'
+
+f = os.popen('diskutil list')
+print f.read()
+
+diskid = raw_input("Type the disk identifier (Something like disk1 or disk1s1): ")
+
+os.system('hdiutil convert -format UDRW -o bootable.img %s' % sys.argv[1])
+os.system('mv bootable.img.dmg bootable.img')
+
+os.system('sudo diskutil unmount /dev/%s' % diskid)
+print 'It is not stuck, just wait until it finishes.'
+print 'Creating bootable usb...'
+os.system('sudo dd bs=1m if=bootable.img of=/dev/r%s' % diskid)
+
+print 'Done!'
